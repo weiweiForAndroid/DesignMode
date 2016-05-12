ec503b4b1eb27e5ae3710f230ed1a95a1bfe1167
wesson <superwesson@totiming.com>
2016-05-12 18:44:21 +0800

    添加单列

---
+++ app/src/main/java/com/totiming/designmode/singleton/SingletonEnum.java
@@ -0,0 +1,23 @@
+package com.totiming.designmode.singleton;
+
+import java.io.ObjectStreamException;
+
+/**
+ * author：supershook on 2016/5/12 18:41
+ * 枚举类单列是线程安全的。
+ */
+public enum SingletonEnum {
+    INSTANCE;
+    public void doSomething(){
+
+    }
+
+    /**
+     * 反序列化的时候会调用该方法。防止破坏单列模式。
+     * @return
+     * @throws ObjectStreamException
+     */
+    private  Object readResolve() throws ObjectStreamException{
+        return  INSTANCE;
+    }
+}
